c458f67f50b5eb88e2e7f7991750876af29e2f56
Azjargal13 <azjargal.gankhuyag@konicaminolta.com>
2020-02-29 15:55:26 +0900

    canvas added

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
+        <span>Let's draw on canvas</span>
+        <!-- <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
@@ -18,7 +19,7 @@
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />
+        />-->
       </div>
 
       <v-spacer></v-spacer>
@@ -34,20 +35,23 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <!-- <HelloWorld /> -->
+      <Main />
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import Main from "./components/Main.vue";
 
 export default Vue.extend({
   name: "App",
 
   components: {
-    HelloWorld
+    // HelloWorld,
+    Main
   },
 
   data: () => ({
---
+++ src/components/Main.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <div>
+          <h1>Ready canvas</h1>
+          <v-card class="d-inline-block mx-auto" elevation="6" :width="1250">
+            <p class="pa-3">Helper little functions for you later :)</p>
+            <canvas
+              id="myCanvas"
+              ref="myCanvas"
+              width="1200"
+              height="1200"
+              style="border:1px solid #000000;"
+            ></canvas>
+          </v-card>
+        </div>
+      </v-col>
+      <v-col>
+        <div>
+          <h1>Canvas tool</h1>
+          <v-stepper vertical>
+            <v-stepper-step step="1" complete
+              >Pick color for canvas border</v-stepper-step
+            >
+
+            <v-stepper-content step="1">
+              <v-color-picker class="ma-2" canvas-height="200"></v-color-picker>
+            </v-stepper-content>
+
+            <v-stepper-step step="2" complete
+              >Pick color for brush</v-stepper-step
+            >
+
+            <v-stepper-content step="2">
+              man
+              <!-- <v-color-picker class="ma-2" canvas-height="200"></v-color-picker> -->
+            </v-stepper-content>
+            <v-stepper-step step="3" complete>Start drawing!</v-stepper-step>
+            <v-stepper-content step="3"
+              >just something functions</v-stepper-content
+            >
+          </v-stepper>
+        </div>
+      </v-col>
+    </v-row>
+
+    <div></div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Main",
+
+  data: () => ({})
+});
+</script>
+
+<style scoped>
+#myCanvas {
+  display: inline;
+  position: relative;
+}
+</style>
